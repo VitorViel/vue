f96a477b87cb272547e29c03358ad65432c2cc96
Vitor Viel <vitorandreviel@gmail.com>
2022-03-10 14:26:23 -0300

    vue work

---
+++ src/adidas/script.js
@@ -0,0 +1,9 @@
+var app = new Vue({
+    el: '#app',
+    data: {
+        product: 'Socks',
+        desc: 'Very confortable and stylish socks for one snow day',
+        image: '../../img/adidas/meiapreta.jpg',
+        qtd: 20,
+    }
+})
---
+++ src/puma/script.js
@@ -0,0 +1,9 @@
+var app = new Vue({
+    el: '#app',
+    data: {
+        product: 'Socks',
+        desc: 'Very confortable and stylish socks for one snow day',
+        image: '../../img/puma/meiapreta.jpg',
+        qtd: 0,
+    }
+})
